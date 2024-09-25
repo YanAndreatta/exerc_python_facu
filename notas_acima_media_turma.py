bd1237aa992aea3e33b51427e7e4bfeb0784fe89
YanAndreatta <yan.duca2@gmail.com>
2024-09-25 00:06:11 -0300

    nota-acima-media-turma

---
+++ notas_acima_media_turma.py
@@ -0,0 +1,15 @@
+value_1 = int(input())
+value_2 = int(input())
+value_3 = int(input())
+value_4 = int(input())
+
+media = (value_1 + value_2 + value_3 + value_4) / 4
+
+if value_1 > media:
+    print(f"{value_1}")
+if value_2 > media:
+    print(f"{value_2}")
+if value_3 > media:
+    print(f"{value_3}")
+if value_4 > media:
+    print(f"{value_4}")
